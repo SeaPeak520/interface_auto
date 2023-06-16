cef79b5825a8eb3c644fe138899e60376220744d
SeaPeak520 <460319422@qq.com>
2023-06-16 11:32:56 +0800

    1

---
+++ main.py
@@ -0,0 +1,32 @@
+import pytest
+import os
+
+from common.file_tools.FromExcelWriteYaml import FromExcelWriteYaml
+from common.file_tools.FromYamlWriteCase import WriteTestCase
+from common.config import ROOT_DIR
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == "__main__":
+    # 读取excel生成yaml用例
+    FromExcelWriteYaml().WriteYamlHandle()
+
+    # 通过yaml用例生成py测试用例
+    WriteTestCase()
+
+    # 执行用例
+    args = [
+        "-s",
+        "-v",
+        "-n=4",
+        "--reruns=2",
+        "--reruns-delay=3",
+        f"{ROOT_DIR}test_case/xiaofa/案源收藏/",
+        f"--alluredir={ROOT_DIR}report/allure_results",
+        "--clean-alluredir"
+    ]
+    #args = ['-s', '-v', '-n=4', '--reruns=2', '--reruns-delay=3', project_path + '/test_case/', '--alluredir=' + project_path + '/report/allure_results', '--clean-alluredir']
+    pytest.main(args)
+    # #
+    # # 生成allure报告
+    # cmd = f"allure generate {ROOT_DIR}report/allure_results -o {ROOT_DIR}report/allure_report -c {ROOT_DIR}report/allure_report"
+    # os.system(cmd)
---
+++ open_report.py
@@ -0,0 +1,4 @@
+import os
+
+cmd = "allure open ./report/allure_report"
+os.system(cmd)
